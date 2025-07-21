c5706b3066f36523c5fb67606ed4df23c72424c3
Ryan Kerr <ryan.kerr@ukaea.uk>
2025-07-21 11:18:02 +0100

    Project now installable using cmake.

---
+++ build/CMakeFiles/DefectStudioCXX.dir/DependInfo.cmake
@@ -0,0 +1,37 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/mnt/c/Users/vp8194/source/repos/DefectStudioCXX/src/Atom.cpp" "CMakeFiles/DefectStudioCXX.dir/src/Atom.cpp.o" "gcc" "CMakeFiles/DefectStudioCXX.dir/src/Atom.cpp.o.d"
+  "/mnt/c/Users/vp8194/source/repos/DefectStudioCXX/src/Cluster.cpp" "CMakeFiles/DefectStudioCXX.dir/src/Cluster.cpp.o" "gcc" "CMakeFiles/DefectStudioCXX.dir/src/Cluster.cpp.o.d"
+  "/mnt/c/Users/vp8194/source/repos/DefectStudioCXX/src/Command.cpp" "CMakeFiles/DefectStudioCXX.dir/src/Command.cpp.o" "gcc" "CMakeFiles/DefectStudioCXX.dir/src/Command.cpp.o.d"
+  "/mnt/c/Users/vp8194/source/repos/DefectStudioCXX/src/CommandArguments.cpp" "CMakeFiles/DefectStudioCXX.dir/src/CommandArguments.cpp.o" "gcc" "CMakeFiles/DefectStudioCXX.dir/src/CommandArguments.cpp.o.d"
+  "/mnt/c/Users/vp8194/source/repos/DefectStudioCXX/src/Copy.cpp" "CMakeFiles/DefectStudioCXX.dir/src/Copy.cpp.o" "gcc" "CMakeFiles/DefectStudioCXX.dir/src/Copy.cpp.o.d"
+  "/mnt/c/Users/vp8194/source/repos/DefectStudioCXX/src/CrystalStructure.cpp" "CMakeFiles/DefectStudioCXX.dir/src/CrystalStructure.cpp.o" "gcc" "CMakeFiles/DefectStudioCXX.dir/src/CrystalStructure.cpp.o.d"
+  "/mnt/c/Users/vp8194/source/repos/DefectStudioCXX/src/DefectStudioCXX.cpp" "CMakeFiles/DefectStudioCXX.dir/src/DefectStudioCXX.cpp.o" "gcc" "CMakeFiles/DefectStudioCXX.dir/src/DefectStudioCXX.cpp.o.d"
+  "/mnt/c/Users/vp8194/source/repos/DefectStudioCXX/src/FileReader.cpp" "CMakeFiles/DefectStudioCXX.dir/src/FileReader.cpp.o" "gcc" "CMakeFiles/DefectStudioCXX.dir/src/FileReader.cpp.o.d"
+  "/mnt/c/Users/vp8194/source/repos/DefectStudioCXX/src/FileWriter.cpp" "CMakeFiles/DefectStudioCXX.dir/src/FileWriter.cpp.o" "gcc" "CMakeFiles/DefectStudioCXX.dir/src/FileWriter.cpp.o.d"
+  "/mnt/c/Users/vp8194/source/repos/DefectStudioCXX/src/Recenter.cpp" "CMakeFiles/DefectStudioCXX.dir/src/Recenter.cpp.o" "gcc" "CMakeFiles/DefectStudioCXX.dir/src/Recenter.cpp.o.d"
+  "/mnt/c/Users/vp8194/source/repos/DefectStudioCXX/src/SetGlobals.cpp" "CMakeFiles/DefectStudioCXX.dir/src/SetGlobals.cpp.o" "gcc" "CMakeFiles/DefectStudioCXX.dir/src/SetGlobals.cpp.o.d"
+  "/mnt/c/Users/vp8194/source/repos/DefectStudioCXX/src/Substitute.cpp" "CMakeFiles/DefectStudioCXX.dir/src/Substitute.cpp.o" "gcc" "CMakeFiles/DefectStudioCXX.dir/src/Substitute.cpp.o.d"
+  "/mnt/c/Users/vp8194/source/repos/DefectStudioCXX/src/Surface.cpp" "CMakeFiles/DefectStudioCXX.dir/src/Surface.cpp.o" "gcc" "CMakeFiles/DefectStudioCXX.dir/src/Surface.cpp.o.d"
+  "/mnt/c/Users/vp8194/source/repos/DefectStudioCXX/src/TaskManager.cpp" "CMakeFiles/DefectStudioCXX.dir/src/TaskManager.cpp.o" "gcc" "CMakeFiles/DefectStudioCXX.dir/src/TaskManager.cpp.o.d"
+  "/mnt/c/Users/vp8194/source/repos/DefectStudioCXX/src/TetrahedralInterstitial.cpp" "CMakeFiles/DefectStudioCXX.dir/src/TetrahedralInterstitial.cpp.o" "gcc" "CMakeFiles/DefectStudioCXX.dir/src/TetrahedralInterstitial.cpp.o.d"
+  "/mnt/c/Users/vp8194/source/repos/DefectStudioCXX/src/Vacancy.cpp" "CMakeFiles/DefectStudioCXX.dir/src/Vacancy.cpp.o" "gcc" "CMakeFiles/DefectStudioCXX.dir/src/Vacancy.cpp.o.d"
+  "/mnt/c/Users/vp8194/source/repos/DefectStudioCXX/src/Volume.cpp" "CMakeFiles/DefectStudioCXX.dir/src/Volume.cpp.o" "gcc" "CMakeFiles/DefectStudioCXX.dir/src/Volume.cpp.o.d"
+  "/mnt/c/Users/vp8194/source/repos/DefectStudioCXX/src/dsutil.cpp" "CMakeFiles/DefectStudioCXX.dir/src/dsutil.cpp.o" "gcc" "CMakeFiles/DefectStudioCXX.dir/src/dsutil.cpp.o.d"
+  "/mnt/c/Users/vp8194/source/repos/DefectStudioCXX/src/global.cpp" "CMakeFiles/DefectStudioCXX.dir/src/global.cpp.o" "gcc" "CMakeFiles/DefectStudioCXX.dir/src/global.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/DefectStudioCXX.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for DefectStudioCXX.
+# This may be replaced when dependencies are built.
